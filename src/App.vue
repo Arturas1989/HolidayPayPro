2bce70665c0c18ccf092252a4aafa440224e6334
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-05-12 16:35:19 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/hollidays">hollidays</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  background: rgb(224, 224, 224);
+}
+nav {
+  display: flex;
+  flex-wrap: wrap;
+  align-self: center;
+}
+
+a {
+  text-decoration: none;
+  padding: 10px 20px;
+  color: black;
+  border-left: 1px solid lightgray;
+  border-right: 1px solid lightgray;
+}
+
+a:hover{
+  background: lightgray;
+  text-decoration: underline;
+}
+
+header a.router-link-exact-active{
+  background: lightcoral;
+}
+
+</style>
