c46d6148ad5395f3bd3822f04e8019d20cc43b44
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-05-13 20:15:40 +0300

    #1 section with SalaryForm component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,22 @@
+
+
 <script setup lang="ts">
+  import SalaryForm from '@/components/SalaryForm.vue';
 </script>
 
 <template>
   <main>
-    
+    <section>
+      <SalaryForm />
+    </section>
   </main>
 </template>
 
 <style>
-  
+  main{
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    margin-top: 20px;
+  }
 </style>
