89f550a44fa9ad3c70f8ab2c91bbe9cf65dd1484
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-05-16 19:09:38 +0300

    clean-up

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -5,7 +5,6 @@ export const getFormFields = (formFields: FormFields) => {
   type Data = {[key: string]: string};
   const data: Data = {};
   for(const key in formFields){
-    console.log(formFields[key].value)
     data[key] = formFields[key].value?.inputRef?.value!;
   }
   return data;
