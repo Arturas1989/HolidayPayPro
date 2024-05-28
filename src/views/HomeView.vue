1a577b2f4e5c06e51dbd69ae24daa43c95bf16d8
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-05-28 16:57:45 +0300

    #11 show results

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
+  import MonthResults from '@/components/MonthResults.vue';
   import SalaryForm from '@/components/SalaryForm.vue';
+  import { computed } from 'vue';
+
+  const model = defineModel<[string, number][]>();
+  const showRef = computed(() => !model.value?.length);
 </script>
 
 <template>
   <main>
     <section>
-      <SalaryForm/>
+      <SalaryForm v-show="showRef"  v-model="model"/>
+      <MonthResults v-show="!showRef"  v-model="model"/>
     </section>
   </main>
 </template>
