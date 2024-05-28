ca9fb17a47bedf5e10f9d984e439defea69f07a7
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-05-28 11:10:13 +0300

    new form related types

--- src/types/FormFields.ts
+++ src/types/FormFields.ts
@@ -1,3 +1,39 @@
-import type { Ref } from "vue";
-import type { DataRef } from '@/types/DataRef';
-export type FormFields = {[key: string] : Ref<DataRef>};
+import type { WorkDays } from '@/helpers/workdays'
+import type { ValidationOptions } from '@/services/Validator'
+import type { Ref } from 'vue'
+import type { DataRef } from './DataRef'
+
+export type SalaryFormFields = {
+  year: unknown
+  salary: unknown
+  days: unknown
+}
+
+export type MonthFields = {
+  [key: string]: string
+}
+
+export type SalaryFields = {
+  [key: string]: string | number
+}
+
+export type FormFields = {
+  workDays: WorkDays
+  monthValidationOptions: ValidationOptions
+  salaryValidationOptions: ValidationOptions
+  monthFields: MonthFields
+  salaryFields: SalaryFields
+}
+
+export type GeneralFields = { [key: string]: Ref<DataRef> }
+
+export type Errors = {
+  [key: string]: string[];
+};
+export type ErrorsRef = Ref<Errors | null>
+
+export type FormErrors = {
+  general: ErrorsRef,
+  month: ErrorsRef,
+  salary: ErrorsRef,
+}
