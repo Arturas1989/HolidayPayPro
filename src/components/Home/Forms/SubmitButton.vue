32529531006b5f221e9d7f47b47f7caf0a84e60a
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-06-05 20:17:49 +0300

    restructured

---
+++ src/components/Home/Forms/SubmitButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button><slot></slot></button>
+</template>
+
+<style scoped>
+button {
+  margin-top: 20px;
+  align-self: flex-start;
+  background-color: rgb(237, 167, 29);
+  color: white;
+  border: none;
+  padding: 7px 15px;
+  border-radius: 15px;
+  cursor: pointer;
+}
+
+button:focus {
+  border: 1px solid black;
+}
+</style>
