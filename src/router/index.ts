45d94131b807e5968ee29eb12be3de6f6ce89dae
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2024-06-09 13:15:06 +0300

    #37 page not found route

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import HolidayView from '@/views/HolidayView.vue'
 import ParamsView from '@/views/ParamsView.vue'
+import NotfoundView from '@/views/NotfoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/params',
       name: 'params',
       component: ParamsView
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotfoundView
     },
   ]
 })
